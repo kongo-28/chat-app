1e2621d14187c9c95c282ceaa4cd4b5987277219
kongo-28 <ryota.tennis.ku@gmail.com>
2024-08-28 15:19:52 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
-  devise_for :users  
-  #自分で追加
-
+  devise_for :users
   root to: "rooms#index"
-
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
